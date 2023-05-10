28598b4d0df868a7dfc689f24d3cc81e7032991e
Abdullah149081 <masudmd425@gmail.com>
2023-05-11 00:08:06 +0600

    footer done

---
+++ src/Page/shared/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import { FaGoogle, FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import logo from "../../../../public/logo.svg";
+
+const Footer = () => {
+  return (
+    <div className=" bg-[#151515] ">
+      <div className="car-container">
+        <footer className="footer p-10  text-white ">
+          <div className="space-y-4">
+            <img src={logo} alt="" />
+            <p className="w-80">Edwin Diaz is a software and web technologies engineer, a life coach trainer who is also a serial.</p>
+            <div className="flex gap-4 ">
+              <FaGoogle className="w-5 h-5" />
+              <FaInstagram className="w-5 h-5" />
+              <FaLinkedinIn className="w-5 h-5" />
+              <FaTwitter className="w-5 h-5" />
+            </div>
+          </div>
+          <div>
+            <span className="footer-title">About</span>
+            <a className="link link-hover">Home</a>
+            <a className="link link-hover">Service</a>
+            <a className="link link-hover">Contact</a>
+          </div>
+          <div>
+            <span className="footer-title">Company</span>
+            <a className="link link-hover">Why Car Doctor</a>
+            <a className="link link-hover">About</a>
+          </div>
+          <div>
+            <span className="footer-title">Support</span>
+            <a className="link link-hover"> Support Center</a>
+            <a className="link link-hover"> Feedback</a>
+            <a className="link link-hover"> Accesbility</a>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/layout/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Footer from "../Page/shared/Footer/Footer";
+
+const Main = () => {
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
