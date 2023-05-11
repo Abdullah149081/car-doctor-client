08dd58ff63ad6de032bcbf09eb0f9c0e10dcfd09
Abdullah149081 <masudmd425@gmail.com>
2023-05-11 18:49:54 +0600

    about us done

---
+++ src/Page/Home/About/About.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import parts from "../../../assets/images/about_us/parts.jpg";
+import person from "../../../assets/images/about_us/person.jpg";
+
+const About = () => {
+  return (
+    <div className="my-20 lg:my-32">
+      <div className="flex flex-col lg:flex-row lg:gap-x-14">
+        <div className="lg:w-1/2 relative">
+          <img src={person} className=" rounded-lg 2xl:w-[630px] lg:w-[460px] lg:h-[473px] object-cover" />
+          <img src={parts} alt={parts} className="w-[327px] h-[332px] absolute lg:right-0  2xl:right-[5%] -bottom-[15%] border-8 border-white rounded-xl object-cover hidden lg:block shadow-lg" />
+        </div>
+        <div className="lg:w-1/2 space-y-6 mt-6 lg:mt-0">
+          <h2 className="text-[#FF3811] font-bold">About Us</h2>
+          <h1 className="text-3xl lg:text-5xl font-bold tracking-wide lg:leading-[4rem]">We are qualified & of experience in this field</h1>
+          <p className="py-4 text-[#737373] ">
+            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomized words which don't look even
+            slightly believable.{" "}
+          </p>
+          <p className="py-4 text-[#737373]">the majority have suffered alteration in some form, by injected humour, or randomized words which don't look even slightly believable. </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/Page/Home/Banner/Banner.jsx
+++ src/Page/Home/Banner/Banner.jsx
@@ -15,10 +15,10 @@ const Banner = () => {
       {banners.map((banner) => (
         <div key={banner.id} id={banner.id} className="carousel-item relative w-full">
           <img src={banner.img_banner} className="rounded-xl w-full lg:h-[600px] 2xl:h-[750px] " alt="banner" />
-          <div className="absolute  text-white  inset-0 rounded-xl  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]   bg-blend-overlay">
+          <div className="absolute  text-white  inset-0 rounded-xl  bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]   ">
             <div className="lg:left-56 top-1/4  lg:top-1/4 left-4 absolute">
               <div className="hidden lg:block space-y-4">
-                <h2 className="text-7xl font-bold w-1/2">Affordable Price For Car Servicing</h2>
+                <h2 className="text-7xl font-bold w-1/2 tracking-wide">Affordable Price For Car Servicing</h2>
                 <p>There are many variations of passages of available, but the majority have suffered alteration in some form</p>
               </div>
               <div className="flex flex-col md:flex-row gap-4 lg:mt-8 ">
